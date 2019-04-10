ae920ce43b3054fbed8697d37d2cfe4323f289ac
Max Trestman <mtrestman1@android-4105f9b98c1d7f96.workspace.codeandsupply.co>
2019-04-10 16:05:41 -0400

    Wrote some initial tests not sure how to implement them yet

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,34 @@
 // Test away!
+import React from 'react';
+import { render, fireEvent } from 'react-testing-library';
+import renderer from 'react-test-renderer';
+import Display from './Display';
+
+describe('<Display>', () => {
+
+    it('should display opened and unlocked as the default properties', () => {
+
+    })
+
+    it('should display green when opened and unlocked', () => {
+
+    })
+
+    it('should display red when closed', () => {
+
+    })
+
+    it('should display red when locked', () => {
+
+    })
+
+    it('should display closed if true otherwise open', () => {
+
+    })
+
+    it('should display locked if true otherwise unlocked', () => {
+
+    })
+
+    
+})
